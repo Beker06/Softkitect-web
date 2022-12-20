649426e54454ecfae14e115906887fbcd10d98a2
Eliam <bosc790@gmail.com>
2022-12-20 12:36:19 -0800

    V0.3.5

--- Components/TechSpecialist/EndBannerTech.jsx
+++ Components/TechSpecialist/EndBannerTech.jsx
@@ -3,22 +3,23 @@ import React from 'react'
 const EndBannerTech = () => {
   return (
     <>
-      <div className="end-tech-banner">
-        <div className="end-tech-banner-row">
-          <div  className="end-tech-banner-text-container">
-            <div className="end-tech-banner-text1" >YOUR REMOTE</div>
-            <div className="end-tech-banner-text2">IT TEAMS</div>
-            <div className="end-tech-banner-text3">AWAITS</div>
-            
-          </div>
-          <div  className="end-tech-banner-info">
-            <div className="end-tech-banner-text4">Your ideas can change the world, but you need a team by your side.
-              We will help you DRAFT, train and champion, your next remote
-              IT team.
+      <div className='black-rectangle'></div>
+        <div className="end-tech-banner">
+          <div className="end-tech-banner-row">
+            <div  className="end-tech-banner-text-container">
+              <div className="end-tech-banner-text1" >YOUR REMOTE</div>
+              <div className="end-tech-banner-text2">IT TEAMS</div>
+              <div className="end-tech-banner-text3">AWAITS</div>
+            </div>
+            <div  className="end-tech-banner-info">
+              <div className="end-tech-banner-text4">Your ideas can change the world, but you need a team by your side.
+                We will help you DRAFT, train and champion, your next remote
+                IT team.
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      <div className='black-rectangle'></div>
     </>
   )
 }
