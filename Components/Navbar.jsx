757b6af7214d1296317b85fd72a235759d2a75a5
Eliam <bosc790@gmail.com>
2023-01-10 15:29:25 -0800

    V0.5.4

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -3,20 +3,24 @@ import Link from 'next/link'
 import { useRouter } from 'next/router';
 import React, { useState } from 'react'
 import LogoHorizontal from "../public/img/softkitect-logo.png";
-
+import BarsIcon from "../public/img/menu.png"
 
 
 const Navbar = () => {
   const router = useRouter();
-  const[navActive, setNavActive] = useState(false);
+  const [navActive, setNavActive] = useState(false);
 
   return (
     <header>
       <nav className='navbar-container'>
+        <div onClick={() => setNavActive(!navActive)}
+          className={`${navActive ? "desplegado" : ""} icon-bars`}>
+          <Image className={`${navActive ? 'desplegado' : ''} icon-bars`} src={BarsIcon} alt="bars-icon" />
+        </div>
+        <Link href="/" className='logo-container'>
+          <Image className='logo' src={LogoHorizontal} alt="softkitec-logo" />
+        </Link>
         <ul className={`${navActive ? 'desplegado' : ''} navLinksContainer`}>
-          <Link href="/">
-            <Image className='logo-container' src={LogoHorizontal} alt="softkitec-logo"/>
-          </Link>
           <li><Link href="/" >
             HOME
           </Link></li>
@@ -40,7 +44,7 @@ const Navbar = () => {
           <li><Link href="/tech-specialist#contact" >
             CONTACT US
           </Link></li>
-          <li><Link href="/tech-specialist#contact" className='get-started-button'>
+          <li><Link href="/tech-specialist#contact" className='get-started-button desplegado'>
             GET STARTED
           </Link></li>
         </ul>
