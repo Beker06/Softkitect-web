5050efc65ac6db63b4361294618c2410b1076161
Eliam <bosc790@gmail.com>
2023-01-12 14:59:42 -0800

    V.0.7.1

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -13,56 +13,58 @@ const Footer = () => {
   return (
     <>
       <footer className="footer-container">
-        <nav className='footer-links-container'>
-          <div className='footer-logo-container'>
-            <Link href="/">
-              <Image src={LogoHorizontal} alt="softkitec-logo2" />
+        <div className='footer-subcontainer'>
+          <nav className='footer-links-container'>
+            <div className='footer-logo-container'>
+              <Link href="/">
+                <Image src={LogoHorizontal} alt="softkitec-logo2" />
+              </Link>
+            </div>
+            <Link href="/" className="inactive margined">
+              HOME
             </Link>
-          </div>
-          <Link href="/" className="inactive">
-            HOME
-          </Link>
-          <Link href="/" className="inactive">
-            SERVICES
-          </Link>
-          <Link href="/#about-us" className="inactive">
-            ABOUT US
-          </Link>
-          <Link href="/it" className="inactive">
-            IT TEAMWORKS
-          </Link>
-          <Link href="/tech-specialist#contact" className="inactive">
-            CONTACT US
-          </Link>
-        </nav>
-        <div className='content-footer'>
-          <div className='col-footer'>
-            <div className='contact-footer-container'>
-              <Image className="icon-footer rotado" src={Cellphone} alt="phone-number"/>
-              <label className='contact-footer-text'>+52 619.363.8880 ext.120</label>
+            <Link href="/" className="inactive">
+              SERVICES
+            </Link>
+            <Link href="/#about-us" className="inactive">
+              ABOUT US
+            </Link>
+            <Link href="/it" className="inactive">
+              IT TEAMWORKS
+            </Link>
+            <Link href="/tech-specialist#contact" className="inactive">
+              CONTACT US
+            </Link>
+          </nav>
+          <div className='content-footer'>
+            <div className='col-footer'>
+              <div className='contact-footer-container margined'>
+                <Image className="icon-footer rotado" src={Cellphone} alt="phone-number"/>
+                <label className='contact-footer-text'>+52 619.363.8880 ext.120</label>
+              </div>
+              <div className='contact-footer-container'>
+                <Image className="icon-footer" src={Envelope} alt="contact-mail"/>
+                <label className='contact-footer-text'>contacto@sofkitect.tech</label>
+              </div>
+              <div className='contact-footer-container'>
+                <Image className="icon-footer" src={Globe} alt="earth-globe"/>
+                <label className='contact-footer-text'>www.sofkitect.tech</label>
+              </div>
+              <div className='contact-footer-container'>
+                <Image className="icon-footer" src={Location} alt="address"/>
+                <label className='contact-footer-text'>Calzada Tecnológico 14487 Local A-11 Parque Internacional Industrial Tijuana, Baja California, México</label>
+              </div>
             </div>
-            <div className='contact-footer-container'>
-              <Image className="icon-footer" src={Envelope} alt="contact-mail"/>
-              <label className='contact-footer-text'>contacto@sofkitect.tech</label>
+            <div className="textfield-footer-container" style={{ display: "flex", flexDirection: "row" }}>
+              <input
+                type="text"
+                name="email"
+                placeholder="YOUR EMAIL"
+              />
+              <button type="submit">
+                SUBMIT
+              </button>
             </div>
-            <div className='contact-footer-container'>
-              <Image className="icon-footer" src={Globe} alt="earth-globe"/>
-              <label className='contact-footer-text'>www.sofkitect.tech</label>
-            </div>
-            <div className='contact-footer-container'>
-              <Image className="icon-footer" src={Location} alt="address"/>
-              <label className='contact-footer-text'>Calzada Tecnológico 14487 Local A-11 Parque Internacional Industrial Tijuana, Baja California, México</label>
-            </div>
-          </div>
-          <div className="textfield-footer-container" style={{ display: "flex", flexDirection: "row" }}>
-            <input
-              type="text"
-              name="email"
-              placeholder="YOUR EMAIL"
-            />
-            <button type="submit">
-              SUBMIT
-            </button>
           </div>
         </div>
         <div className='copyright-container'>
