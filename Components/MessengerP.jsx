dc5fc285f357d400abb533de4b9d9695822762c7
Eliam <bosc790@gmail.com>
2023-01-21 12:10:05 -0800

    V0.11.1

--- Components/MessengerP.jsx
+++ Components/MessengerP.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import MessengerCustomerChat from 'react-messenger-customer-chat/lib/MessengerCustomerChat'
+// import React from 'react'
+// import MessengerCustomerChat from 'react-messenger-customer-chat/lib/MessengerCustomerChat'
 
-const MessengerP = () => {
-  return (
-    <>
-        <MessengerCustomerChat pageId="776351096036013" appId="376914473665418" />
-    </>
-  )
-}
+// const MessengerP = () => {
+//   return (
+//     <>
+//         <MessengerCustomerChat pageId="776351096036013" appId="376914473665418" />
+//     </>
+//   )
+// }
 
-export default MessengerP
+// export default MessengerP
